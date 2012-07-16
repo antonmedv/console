2bdad66e1a0493ea968e8c8bc50787999d435160
Elfet <anton@elfet.ru>
2012-07-16 19:13:30 +0400

    first commit

--- git.php
+++ git.php
@@ -1,6 +1,6 @@
 <?php
 ignore_user_abort(true);
 set_time_limit(0);
-$git = '/home/u77602/git/git/bin/git';
-$output = shell_exec("cd ../chat/ && $git pull");
+$git = 'git';
+$output = shell_exec("cd ./ && $git pull");
 echo "<pre>$output</pre>";
