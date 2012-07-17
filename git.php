1af0383bd62c5bedaba7ba89549d3d19bd98847d
Elfet <anton@elfet.ru>
2012-07-17 23:24:10 +0400

    Mask * (any) for allow and deny. Remove extra space from html.

--- git.php
+++ git.php
@@ -18,15 +18,27 @@ if (!empty($userCommand)) {
         $options = array_merge($options, $userOptions);
     }
 
+    function searchCommand($command, $array)
+    {
+        $inArray = false;
+        foreach ($array as $pattern) {
+            $pattern = str_replace('\*', '?(.*?)', preg_quote($pattern));
+            if (preg_match("/^$pattern$/i", $command)) {
+                $inArray = true;
+            }
+        }
+        return $inArray;
+    }
+
     if (is_array($options['allow'])) {
-        if (!in_array($userCommand, $options['allow'])) {
+        if (!searchCommand($userCommand, $options['allow'])) {
             $these = implode('<br>', $options['allow']);
             die("<span class='error'>Sorry, but this command not allowed. Try these:<br>{$these}</span><br>");
         }
     }
 
     if (is_array($options['deny'])) {
-        if (in_array($userCommand, $options['deny'])) {
+        if (searchCommand($userCommand, $options['deny'])) {
             die("<span class='error'>Sorry, but this command is denied.</span><br>");
         }
     }
@@ -71,223 +83,223 @@ if (!empty($userCommand)) {
 <!doctype html>
 <html>
 <head>
-    <title>php-git</title>
-    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-    <style type="text/css">
-        * {
-            margin: 0;
-            padding: 0;
-        }
+<title>php-git</title>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<style type="text/css">
+    * {
+        margin: 0;
+        padding: 0;
+    }
 
-        body {
-            padding: 10px;
-        }
+    body {
+        padding: 10px;
+    }
 
-        form {
-            white-space: nowrap;
-        }
+    form {
+        white-space: nowrap;
+    }
 
-        input {
-            border: none;
-            outline: none;
-            width: 500px;
-        }
+    input {
+        border: none;
+        outline: none;
+        width: 500px;
+    }
 
-        input:focus {
-            outline: none;
-        }
+    input:focus {
+        outline: none;
+    }
 
-        pre,
-        form,
-        input {
-            color: #333333;
-            font-family: ‘ Lucida Console ’, Monaco, monospace;
-            font-size: 16px;
-        }
+    pre,
+    form,
+    input {
+        color: #333333;
+        font-family: 'Lucida Console', Monaco, monospace;
+        font-size: 16px;
+    }
 
-        pre {
-            white-space: pre;
-        }
+    pre {
+        white-space: pre;
+    }
 
-        span {
-            color: blue;
-        }
+    span {
+        color: blue;
+    }
 
-        span.error {
-            color: red;
-        }
+    span.error {
+        color: red;
+    }
 
-        span.autocomplete span.guess {
-            color: #a9a9a9;
-        }
-    </style>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript">
-        /**
-         *  History of commands.
-         */
-        (function ($) {
-            var maxHistory = 100;
-            var position = -1;
-            var currentCommand = '';
-            var addCommand = function (command) {
-                var ls = localStorage['commands'];
-                var commands = ls ? JSON.parse(ls) : [];
-                if (commands.length > maxHistory) {
-                    commands.shift();
+    span.autocomplete span.guess {
+        color: #a9a9a9;
+    }
+</style>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript">
+    /**
+     *  History of commands.
+     */
+    (function ($) {
+        var maxHistory = 100;
+        var position = -1;
+        var currentCommand = '';
+        var addCommand = function (command) {
+            var ls = localStorage['commands'];
+            var commands = ls ? JSON.parse(ls) : [];
+            if (commands.length > maxHistory) {
+                commands.shift();
+            }
+            commands.push(command);
+            localStorage['commands'] = JSON.stringify(commands);
+        };
+        var getCommand = function (at) {
+            var ls = localStorage['commands'];
+            var commands = ls ? JSON.parse(ls) : [];
+            if (at < 0) {
+                position = at = -1;
+                return currentCommand;
+            }
+            if (at >= commands.length) {
+                position = at = commands.length - 1;
+            }
+            return commands[commands.length - at - 1];
+        };
+
+        $.fn.history = function () {
+            var input = $(this);
+            input.keydown(function (e) {
+                var code = (e.keyCode ? e.keyCode : e.which);
+
+                if (code == 38) { // Up
+                    if (position == -1) {
+                        currentCommand = input.val();
+                    }
+                    input.val(getCommand(++position));
+                    return false;
+                } else if (code == 40) { // Down
+                    input.val(getCommand(--position));
+                    return false;
+                } else {
+                    position = -1;
                 }
-                commands.push(command);
-                localStorage['commands'] = JSON.stringify(commands);
-            };
-            var getCommand = function (at) {
-                var ls = localStorage['commands'];
-                var commands = ls ? JSON.parse(ls) : [];
-                if (at < 0) {
-                    position = at = -1;
-                    return currentCommand;
-                }
-                if (at >= commands.length) {
-                    position = at = commands.length - 1;
-                }
-                return commands[commands.length - at - 1];
-            };
+            });
 
-            $.fn.history = function () {
-                var input = $(this);
-                input.keydown(function (e) {
-                    var code = (e.keyCode ? e.keyCode : e.which);
+            return input;
+        };
 
-                    if (code == 38) { // Up
-                        if (position == -1) {
-                            currentCommand = input.val();
+        $.fn.addHistory = function (command) {
+            addCommand(command);
+        };
+    })(jQuery);
+
+    /**
+     * Autocomplete input.
+     */
+    (function ($) {
+        $.fn.autocomplete = function (commands) {
+            var input = $(this);
+            input.wrap('<span class="autocomplete" style="position: relative;"></span>');
+            var html =
+                '<span class="overflow" style="position: absolute; z-index: 10;">' +
+                    '<span class="repeat" style="opacity: 0;"></span>' +
+                    '<span class="guess"></span></span>';
+            $('.autocomplete').prepend(html);
+            var repeat = $('.repeat');
+            var guess = $('.guess');
+            var search = function (text) {
+                var commandFound = '';
+                if (text != '') {
+                    for (var i = 0; i < commands.length; i++) {
+                        var command = commands[i];
+                        if (command.length > text.length &&
+                            command.substring(0, text.length) == text) {
+                            commandFound = command.substring(text.length, command.length);
+                            break;
                         }
-                        input.val(getCommand(++position));
-                        return false;
-                    } else if (code == 40) { // Down
-                        input.val(getCommand(--position));
-                        return false;
-                    } else {
-                        position = -1;
                     }
-                });
-
-                return input;
+                }
+                guess.text(commandFound);
+                return text;
             };
-
-            $.fn.addHistory = function (command) {
-                addCommand(command);
+            var update = function () {
+                repeat.text(search(input.val()));
             };
-        })(jQuery);
+            input.change(update);
+            input.keyup(update);
+            input.keypress(update);
+            input.keydown(update);
 
-        /**
-         * Autocomplete input.
-         */
-        (function ($) {
-            $.fn.autocomplete = function (commands) {
-                var input = $(this);
-                input.wrap('<span class="autocomplete" style="position: relative;"></span>');
-                var html =
-                    '<span class="overflow" style="position: absolute; z-index: 10;">' +
-                        '<span class="repeat" style="opacity: 0;"></span>' +
-                        '<span class="guess"></span></span>';
-                $('.autocomplete').prepend(html);
-                var repeat = $('.repeat');
-                var guess = $('.guess');
-                var search = function (text) {
-                    var commandFound = '';
-                    if (text != '') {
-                        for (var i = 0; i < commands.length; i++) {
-                            var command = commands[i];
-                            if (command.length > text.length &&
-                                command.substring(0, text.length) == text) {
-                                commandFound = command.substring(text.length, command.length);
-                                break;
-                            }
-                        }
-                    }
-                    guess.text(commandFound);
-                    return text;
-                };
-                var update = function () {
-                    repeat.text(search(input.val()));
-                };
-                input.change(update);
-                input.keyup(update);
-                input.keypress(update);
-                input.keydown(update);
-
-                input.keydown(function (e) {
-                    var code = (e.keyCode ? e.keyCode : e.which);
-                    if(code == 9) {
-                        var val = input.val();
-                        input.val(val + guess.text());
-                        return false;
-                    }
-                });
-
-                return input;
-            };
-        })(jQuery);
-
-        /**
-         * Init console.
-         */
-        $(function () {
-            var screen = $('pre');
-            var input = $('input').focus();
-            var form = $('form');
-            var scroll = function () {
-                window.scrollTo(0, document.body.scrollHeight);
-            };
-            input.history();
-            input.autocomplete([
-                'status',
-                'push',
-                'pull',
-                'add',
-                'bisect',
-                'branch',
-                'checkout',
-                'clone',
-                'commit',
-                'diff',
-                'fetch',
-                'grep',
-                'init',
-                'log',
-                'merge',
-                'mv',
-                'rebase',
-                'reset',
-                'rm',
-                'show',
-                'tag'
-            ]);
-            form.submit(function () {
-                var command = $.trim(input.val());
-                if (command == '') {
+            input.keydown(function (e) {
+                var code = (e.keyCode ? e.keyCode : e.which);
+                if (code == 9) {
+                    var val = input.val();
+                    input.val(val + guess.text());
                     return false;
                 }
+            });
 
-                $("<span>&rsaquo; git " + command + "</span><br>").appendTo(screen);
-                scroll();
-                input.val('');
-                form.hide();
-                input.addHistory(command);
+            return input;
+        };
+    })(jQuery);
 
-                $.get('?' + command, function (output) {
-                    screen.append(output);
-                    form.show();
-                    scroll();
-                });
+    /**
+     * Init console.
+     */
+    $(function () {
+        var screen = $('pre');
+        var input = $('input').focus();
+        var form = $('form');
+        var scroll = function () {
+            window.scrollTo(0, document.body.scrollHeight);
+        };
+        input.history();
+        input.autocomplete([
+            'status',
+            'push',
+            'pull',
+            'add',
+            'bisect',
+            'branch',
+            'checkout',
+            'clone',
+            'commit',
+            'diff',
+            'fetch',
+            'grep',
+            'init',
+            'log',
+            'merge',
+            'mv',
+            'rebase',
+            'reset',
+            'rm',
+            'show',
+            'tag'
+        ]);
+        form.submit(function () {
+            var command = $.trim(input.val());
+            if (command == '') {
                 return false;
-            });
+            }
 
-            $(document).click(function () {
-                input.focus();
+            $("<span>&rsaquo; git " + command + "</span><br>").appendTo(screen);
+            scroll();
+            input.val('');
+            form.hide();
+            input.addHistory(command);
+
+            $.get('?' + command, function (output) {
+                screen.append(output);
+                form.show();
+                scroll();
             });
+            return false;
         });
-    </script>
+
+        $(document).click(function () {
+            input.focus();
+        });
+    });
+</script>
 </head>
 <body>
 <pre></pre>
